86da751adecf0b9456d0ece898e03082985725ee
Alex Moskov <112842147+Aleantmos@users.noreply.github.com>
2022-12-09 12:19:37 +0200

    Add files via upload

---
+++ _1_Abstraction/_5_DayOfWeek/Main.java
@@ -0,0 +1,9 @@
+package _5_DayOfWeek;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        DayOfWeek dayOfWeek = DayOfWeek.THURSDAY;
+        System.out.println(dayOfWeek.getDayNumber());
+    }
+}

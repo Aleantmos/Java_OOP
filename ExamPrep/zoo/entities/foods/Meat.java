732b567cefd455dcb2624689b6b6507516316806
Alex Moskov <112842147+Aleantmos@users.noreply.github.com>
2022-12-27 13:51:14 +0200

    Add files via upload

---
+++ ExamPrep/zoo/entities/foods/Meat.java
@@ -0,0 +1,12 @@
+package zoo.entities.foods;
+
+public class Meat extends BaseFood {
+
+    private static final int CALORIES = 10;
+    private static final double PRICE = 7.00;
+
+
+    public Meat() {
+        super(CALORIES, PRICE);
+    }
+}

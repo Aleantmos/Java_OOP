bbf8aaebf955b6417eb8612b351f0328a72fa22f
Alex Moskov <112842147+Aleantmos@users.noreply.github.com>
2023-01-02 11:08:48 +0200

    Add files via upload

---
+++ Exams/09.04.2022/fairyShop/models/Present/Present.java
@@ -0,0 +1,11 @@
+package fairyShop.models.Present;
+
+public interface Present {
+    String getName();
+
+    int getEnergyRequired();
+
+    boolean isDone();
+
+    void getCrafted();
+}
---
+++ Exams/09.04.2022/fairyShop/models/Shop/Shop.java
@@ -0,0 +1,8 @@
+package fairyShop.models.Shop;
+
+import fairyShop.models.Helper.Helper;
+import fairyShop.models.Present.Present;
+
+public interface Shop {
+    void craft(Present present, Helper helper);
+}

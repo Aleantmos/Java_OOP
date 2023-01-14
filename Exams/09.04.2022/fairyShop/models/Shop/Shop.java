db9b8efae80dad0b0e9d7dfa82855f5395a20ead
Alex Moskov <112842147+Aleantmos@users.noreply.github.com>
2023-01-14 19:28:28 +0200

    Delete Exams/09.04.2022/fairyShop directory

--- Exams/09.04.2022/fairyShop/models/Shop/Shop.java
+++
@@ -1,8 +0,0 @@
-package fairyShop.models.Shop;
-
-import fairyShop.models.Helper.Helper;
-import fairyShop.models.Present.Present;
-
-public interface Shop {
-    void craft(Present present, Helper helper);
-}
